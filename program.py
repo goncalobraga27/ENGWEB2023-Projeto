420f1d67fe659235791ec03e23f4dd8ab52665ba
goncalobraga27 <a97541@alunos.uminho.pt>
2023-04-04 22:49:27 +0100

    Ligações entre registos

--- program.py
+++ program.py
@@ -1,100 +1,96 @@
-import csv 
+import csv
 import json
 import re
-from time import sleep
+import time
 
-"""
-Notas a rever :
-- Maneira como tratamos das ligacoes -> colocar mais eficiente
-- Ingnorar registos que nao nos interessam (depois rever este parametro com a api de dados)
+def makeJson(cabecalho, rows):
+    escrita = 0
+    f = open('data/db.json', 'w', encoding="utf-8")
+    cabecalho = ['_id'] + cabecalho
+    listaFicheiro = []
+    dicLigacoes = dict()
+    auxdicLigacoes = dict()
+    while escrita != 3:
+        if escrita == 0:
+            for row in rows:
+                paramLinha = ""
+                for it in row:
+                    paramLinha += it
+                paramLinha = paramLinha.split(";")
+                if len(paramLinha) >= 4 and paramLinha[4] !='':
+                    number = paramLinha[4]
+                    number = number[1:]
+                    number = number[:-1]
+                    auxdicLigacoes[int(number)] = paramLinha[0]
+            escrita += 1
+        if escrita == 1:
+            for row in rows:
+                listaOId = []
+                paramLinha = ""
+                nameProcess = ""
+                filiacaoProcess = ""
+                numberOtherProcess = ""
+                for it in row:
+                    paramLinha += it
+                searchNameProcess = re.search(r"Inquirição de genere de [a-zA-Z\s]+(\")?", paramLinha)
+                searchFiliacaoProcess = re.search(r"Filiação:[a-zA-Z\s]+\.", paramLinha)
+                if searchNameProcess:
+                    nameProcess = searchNameProcess.group()[24:]
+                    nameProcess = nameProcess[:-1]
+                if searchFiliacaoProcess:
+                    filiacaoProcess = searchFiliacaoProcess.group()[10:]
+                    filiacaoProcess = filiacaoProcess[:-1]
+                    filiacaoProcess = filiacaoProcess.split(" e ")
+                paramLinha = paramLinha.split(";")
+                if len(paramLinha) >= 63:
+                    forligacao = paramLinha[63]
+                    searchOtherProcess = re.findall(r"\d+", forligacao)
+                    if searchOtherProcess:
+                        for it in searchOtherProcess:
+                            if int(it) in auxdicLigacoes:
+                                listaOId.append(auxdicLigacoes[int(it)])
+                if nameProcess != "":
+                    dicLigacoes[nameProcess] = (paramLinha[0], filiacaoProcess, listaOId)
+            escrita += 1
+        elif escrita == 2:
+            for row in rows:
+                dic = dict()
+                paramLinha = ""
+                nameProcess = ""
+                listaId = []
+                for it in row:
+                    paramLinha += it
+                searchNameProcess = re.search(r"Inquirição de genere de [a-zA-Z\s]+(\")?", paramLinha)
+                if searchNameProcess:
+                    nameProcess = searchNameProcess.group()[24:]
+                    nameProcess = nameProcess[:-1]
+                paramLinha = paramLinha.split(";")
+                if nameProcess != "":
+                    listaP = (dicLigacoes[nameProcess])[1]
 
-Pensar de quais dados deste csv e` que sao importantes para isto
+                    for it in listaP:
+                        if it in dicLigacoes:
+                            listaId.append((dicLigacoes[it])[0])
+                    listaId += (dicLigacoes[nameProcess])[2]
 
--- tratar bem os dados (essencial para depois mandar menos cenas ao servidor)
+                if len(paramLinha) > len(cabecalho):
+                    for i in range(len(cabecalho) - 1):
+                        dic[cabecalho[i]] = paramLinha[i]
+                    dic["ligacoes"] = listaId
+                    listaFicheiro.append(dic)
+                else:
+                    for i in range(len(paramLinha) - 1):
+                        dic[cabecalho[i]] = paramLinha[i]
+                    dic["ligacoes"] = listaId
+                    listaFicheiro.append(dic)
 
-falar com o ramalho-> perguntar duvidas do tratamento do dataset
-sendo isto uma disciplina de ew vamos gastar mais tempo em tratamento do dataset
-mas tratar -lo vai levar a um desempenho melhor do servidor
+            json.dump(listaFicheiro, f, indent=" ")
+            f.close()
+            escrita += 1
 
-"""
-
-
-def ligacoes(lig,fjson):
-    #print(lig)
-    res = list()
-    
-
-
-    i = 0
-    for l in lig: #mudar isto por amor de deus
-        laux = {'ligacoes' : {}}
-        for n in l.keys():
-            uax = dict()
-            for nomes in l[n]:
-                for e in fjson:
-                    #print(e['UnitTitle'])
-                    try :
-                        if re.search(e['UnitTitle'],nomes) :
-                            laux['ligacoes'].update({e["_id"]:nomes})
-                    except:
-                        print(e) 
-
-            
-        fjson[i].update(laux)
-        res += [fjson[i]] 
-        #print(res)
-        #sleep(4)
-        i+=1
-
-    return res
-
-
-
-def makeJson(cabecalho,rows):
-    rgx = re.compile(r"(?<=Inquirição de genere de )(\w+|\s)*")
-    f = open('data/db.json','w',encoding="utf-8")
-    listaFicheiro=[]
-    listaligacoes=[]
-    cabecalho = ['_id']+cabecalho
-    #print(cabecalho[7])
-    #print(cabecalho.index("ScopeContent")) == 38
-    for row in rows:
-        dic = dict()
-        dicligacoes = dict()
-        paramLinha =""
-        for it in row:
-            paramLinha +=it
-        paramLinha = paramLinha.split(";")
-        if len(paramLinha) > len(cabecalho):
-            for i in range(len(cabecalho)-1):
-                #rgx = r"(?<=Inquirição de genere de )(\w+|\s)*" #tratar da informacao
-                dic[cabecalho[i]] = paramLinha[i]
-            listaFicheiro.append(dic)
-        else:
-            for i in range(len(paramLinha)-1):
-                dic[cabecalho[i]] = paramLinha[i]
-            listaFicheiro.append(dic)
-        
-        if (len(paramLinha) > 63):
-            #print(paramLinha[8])
-            """
-                TRATAR DO DATASET -> RelatedMaterial !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            """
-            r = re.search(rgx, paramLinha[8]).group()
-            if(paramLinha[38] != ""):
-                try :
-                    rg = re.search(r"(?<=Filiação: )(\w+|\s)*(\w+|\s)*",paramLinha[38]).group().split(" e ")
-                except:
-                    pass
-                dicligacoes[r] = rg
-                listaligacoes.append(dicligacoes) 
-    res = ligacoes(listaligacoes,listaFicheiro)
-    json.dump(res, f,indent=" ")
-    f.close()
-    
 
 def main():
-    f = open("data/registos.csv","r",encoding="utf-8")
+    f = open("data/registos.csv", "r", encoding="utf-8")
     csvreader = csv.reader(f)
     headerFile = next(csvreader)
     rows = []
@@ -103,8 +99,9 @@ def main():
     cabecalho = headerFile[0]
     cabecalho = cabecalho.split(";")
     cabecalho = cabecalho[1:]
-    makeJson(cabecalho,rows[3:]) # ignora as 4 primeiras linhas (sao palha)
+    makeJson(cabecalho, rows)
     f.close()
+
 
 if __name__ == "__main__":
     main()
