772c7b97c308a52c17a0dae82c7995226880810b
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2023-06-22 03:16:00 +0100

    indice de registos quase done

--- mainServer/public/javascript/dynamic.js
+++ mainServer/public/javascript/dynamic.js
@@ -3,27 +3,6 @@ $(function () {
 
 })
 
-/*
-function getOrder() {
-    var id;
-    var original_link = "";
-    $('#order').on('change', function(){
-            //$('.apply').attr('href', original_link);
-            id = $(this).val();
-            var new_href = $('.apply').attr('href') + id;
-            //$('.apply').attr('href', new_href);
-            document.querySelector('#order').setAttribute("href", new_href);
-    });
-}
-*/
-/*
-$('apply').on('click', function() {
-    var text = $('#myInput').val() + ' ' + $('#mySelect').val();
-    alert(text);
-  })
-  */
-
-
 $(document).on('change', '#order', function() {
     var value = $(this).val();
     console.log(value)
