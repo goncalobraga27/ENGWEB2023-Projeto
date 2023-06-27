d77e91fef68d065667bc847e40d429c7e466ac50
simaobarroso <simao.barroso@outlook.pt>
2023-06-27 16:51:06 +0100

    adicao da pesquisa rapida (falta na api)

--- mainServer/routes/index.js
+++ mainServer/routes/index.js
@@ -477,6 +477,19 @@ router.post('/processos/:id/addLigacoes',verificaToken, function(req, res, next)
     })
 });
 
+/* GET /pesquisaRapida/:exp */
+router.get('/pesquisaRapida/:exp',verificaToken, function(req, res, next) {
+  var data = new Date().toISOString().substring(0, 16);
+  axios.get(env.authAccessPoint+"/regexSearch/"+req.params.exp+"?token=" + req.cookies.token)
+    .then(dados => {
+      res.render('searchList', {u: dados.data, d: data });
+    })
+  .catch(erro => {
+      res.render('error', {error: erro, message: "Erro no dados da rota GET /pesquisaRapida/:exp"})
+  })
+   
+});
+
 /* GET /search */
 router.get('/search',verificaToken, function(req, res, next) {
   var data = new Date().toISOString().substring(0, 16);
